73279527f2f593fa3073dd19bc0aca894cc96fcd
davidg <david.dev.train@gmail.com>
2022-02-08 17:47:01 -0500

    added authentication with socketio

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const session = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 
 
@@ -19,6 +24,14 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
+
+// set views directory and view engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+
+
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -27,6 +40,8 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
+  store: store,
+  key: 'express.sid',
   cookie: { secure: false }
 }));
 app.use(passport.initialize());
@@ -34,9 +49,16 @@ app.use(passport.session());
 
 
 
-// set views directory and view engine
-app.set('views', './views');
-app.set('view engine', 'pug');
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 
 
@@ -51,16 +73,12 @@ myDB(async client => {
 
   io.on('connection', socket => {
 
-    console.log('A user has connected');
-    
-    socket.on('connect', () => {
-      ++currentUsers;
-      io.emit('user count', currentUsers);
-    });
+    console.log('user ' + socket.request.user.name + ' connected');
+
+    io.emit('user count', ++currentUsers);
 
     socket.on('disconnect', () => {
-      --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user count', --currentUsers);
     });
 
   });
@@ -82,3 +100,17 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io: ', message);
+
+  accept(null, false);
+}
